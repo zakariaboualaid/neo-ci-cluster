8aa72d3657835316953213b39c1c4b006dbd3944
Zakaria Boualaid <boulaidzac@gmail.com>
2020-01-25 07:01:16 +0100

    fix format

--- terraform/Modules/DevopsTower/main.tf
+++ terraform/Modules/DevopsTower/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+	region = "us-east-2"
 }
 
 resource "aws_instance" "devops_tower" {
@@ -8,28 +8,28 @@ resource "aws_instance" "devops_tower" {
 	availability_zone = local.availability_zone
 	tags = {
 		Name = "DevOps Tower",
-    Role = "Management",
-    Guru = "Zakaria Boualaid"
+		Role = "Management",
+		Guru = "Zakaria Boualaid"
 	}
 	iam_instance_profile = "DevOpsTower"
-  key_name = "admin-key"
+	key_name = "admin-key"
 	subnet_id = aws_subnet.subnet-neo-1.id
 	security_groups = [aws_security_group.ingress-all.id]
 	root_block_device {
 		volume_size = 20
 	}
-  provisioner "remote-exec" {
+	provisioner "remote-exec" {
 		inline = ["echo 'hello neo'"]
 		
 		connection {
 			type = "ssh"
 			user = local.ssh_user
-      host = aws_instance.devops_tower.public_ip
+			host = aws_instance.devops_tower.public_ip
 			private_key = file(local.private_key_path)
 		}
-  }
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ${aws_instance.devops_tower.public_ip} --private-key ${local.private_key_path} ../ansible/devops_tower.yml" 
+	}
+	provisioner "local-exec" {
+		command = "ansible-playbook -i ${aws_instance.devops_tower.public_ip} --private-key ${local.private_key_path} ../ansible/devops_tower.yml" 
   }
 }
 
@@ -38,14 +38,14 @@ resource "aws_ebs_volume" "mgmt_srv_volume" {
 	size              = 30
 	tags = {
 		Name = "DevOps Tower EBS",
-    Role = "Management",
-    Guru = "Zakaria Boualaid"
+		Role = "Management",
+		Guru = "Zakaria Boualaid"
 	}
 }
 
 resource "aws_volume_attachment" "mgmt_srv_ebs_att" {
 	device_name = "/dev/sdh"
- 	volume_id   = aws_ebs_volume.mgmt_srv_volume.id
+	volume_id   = aws_ebs_volume.mgmt_srv_volume.id
 	instance_id = aws_instance.devops_tower.id
 }
 
@@ -71,18 +71,18 @@ resource "aws_security_group" "ingress-all" {
 	name = "allow-all-sg"
 	vpc_id = aws_vpc.neo.id
 	ingress {
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+		cidr_blocks = [
+			"0.0.0.0/0"
+		]
 		from_port = 22
-    to_port = 22
-    protocol = "tcp"
-  }
-  egress {
-   from_port = 0
-   to_port = 0
-   protocol = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
+		to_port = 22
+		protocol = "tcp"
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
  	}
 }
 
