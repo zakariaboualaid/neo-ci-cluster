a00b5fa38d95459aece9727b10bfc9d2171b78f5
Zakaria Boualaid <boulaidzac@gmail.com>
2020-01-27 03:28:07 +0100

    add aws provider to helm

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,35 +20,41 @@ module "acm" {
 
 provider "helm" {
   install_tiller  = true
-  service_account = "tiller"
+  service_account = ""
   namespace = "kube-system"
 }
 
-#resource "kubernetes_service_account" "tiller" {
-  #metadata {
-    #name = "tiller"
-    #namespace = "kube-system"
-  #}
-#}
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name = "tiller"
+    namespace = "kube-system"
+  }
+}
 
-#resource "kubernetes_cluster_role_binding" "tiller_cluster_role" {
-  #metadata {
-    #name = "tiller"
+resource "kubernetes_cluster_role_binding" "tiller_cluster_role" {
+  metadata {
+    name = "tiller"
 
-  #}
+  }
 
-  #role_ref {
-    #api_group = "rbac.authorization.k8s.io"
-    #kind = "ClusterRole"
-    #name = "cluster-admin"
-  #}
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
 
-  #subject {
-    #kind = "ServiceAccount"
-    #name = kubernetes_service_account.tiller.metadata.0.name
-    #namespace = "kube-system"
-  #}
+  subject {
+    kind = "ServiceAccount"
+    name = "default"
+    namespace = "kube-system"
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = kubernetes_service_account.tiller.metadata.0.name
+    namespace = "kube-system"
+  }
 		
-#}
+}
 
 
