17ce9000c350d112e5fdfb2154f7dfff505d6b3e
Zakaria Boualaid <boulaidzac@gmail.com>
2020-01-27 02:56:10 +0100

    add aws provider to helm

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,38 @@ module "acm" {
   domain_name = trimsuffix(data.aws_route53_zone.this.name, ".")
   zone_id     = data.aws_route53_zone.this.id
 }
+
+provider "helm" {
+  install_tiller  = true
+  service_account = kubernetes_service_account.tiller.metadata.0.name
+  namespace = "kube-system"
+}
+
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "tiller_cluster_role" {
+  metadata {
+    name = "tiller"
+
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = kubernetes_service_account.tiller.metadata.0.name
+    namespace = "kube-system"
+  }
+		
+}
+
+
